7dcf25ea0b38b9b87df0cca9a0d3878538aa72fa
ot-kaz <otsrptkd@gmail.com>
2020-02-03 18:53:35 +0900

    view change2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -98,18 +98,19 @@ $('#new_message').on('submit', function(e){
     } else if (message.image) {
       //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
-            message.user_name +
+        `<div class="message-box">` +
+          `<div class="upper-message">` +
+            `<div class="upper-message__user-name">` +
+              message.user_name +
+            `</div>` +
+            `<div class="upper-message__date">` +
+              message.created_at +
+            `</div>` +
           `</div>` +
-          `<div class="upper-message__date">` +
-            message.created_at +
+          `<div class="lower-message">` +
+            `<img src="` + message.image + `" class="lower-message__image" >` +
           `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+        `</div>`
     };
     return html;
   };
