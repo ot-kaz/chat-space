f74abbbfc64cf55c46b86fbc2469ff653336c2f7
ot-kaz <otsrptkd@gmail.com>
2020-02-03 17:20:50 +0900

    view-change

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -12,11 +12,12 @@ $('#new_message').on('submit', function(e){
     processData: false,
     contentType: false
   })
+ 
    .done(function(data){
      var html = buildHTML(data);
-     $('.messages').append(html);      
+     $('.messages').append(html);     
      $('form')[0].reset();
-     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight},'fast');
+     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
      $(".form__submit").attr('disabled', false);
    })
   })
@@ -32,6 +33,7 @@ $('#new_message').on('submit', function(e){
       type: 'get',
       dataType: 'json',
       //dataオプションでリクエストに値を含める
+      
       data: {id: last_message_id}
     })
     .done(function(messages) {
@@ -42,7 +44,6 @@ $('#new_message').on('submit', function(e){
         var insertHTML = '';
         //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
-          
           insertHTML += buildHTML(message)
         });
         //メッセージが入ったHTMLに、入れ物ごと追加
@@ -64,38 +65,36 @@ $('#new_message').on('submit', function(e){
     if (message.content && message.image) {
       //data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
+        `<div class="message-box">` +
+          `<div class="message_updata_user">` +
             message.user_name +
           `</div>` +
           `<div class="upper-message__date">` +
             message.created_at +
+          `</div>` + +
+          `<div class="lower-message">` +
+            `<p class="lower-message__content">` +
+              message.content +
+            `</p>` +
+            `<img src="` + message.image + `" class="lower-message__image" >` +
           `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-          `<img src="` + message.image + `" class="lower-message__image" >` +
-        `</div>` +
-      `</div>`
+        `</div>`
     } else if (message.content) {
       //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
-        `<div class="upper-message">` +
-          `<div class="upper-message__user-name">` +
+        `<div class="message-box">` +
+          `<div class="message_updata_user">` +
             message.user_name +
           `</div>` +
           `<div class="upper-message__date">` +
             message.created_at +
           `</div>` +
-        `</div>` +
-        `<div class="lower-message">` +
-          `<p class="lower-message__content">` +
-            message.content +
-          `</p>` +
-        `</div>` +
-      `</div>`
+          `<div class="lower-message">` +
+            `<p class="lower-message__content">` +
+              message.content +
+            `</p>` +
+          `</div>` +
+        `</div>`
     } else if (message.image) {
       //同様に、data-idが反映されるようにしている
       var html = `<div class="message" data-message-id=` + message.id + `>` +
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,8 +17,13 @@ class GroupsController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @group = Group.find(params[:id])
+  end
 
   def update
+    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to root_path, notice: 'グループを更新しました'
     else
